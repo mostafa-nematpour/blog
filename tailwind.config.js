511febbed34d989940c7cdbe76b40d50c9b859b1
mostafa <nematpor46@gmail.com>
2024-02-15 19:59:16 +0330

    feat: add font `vazirmatn`

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'vazirmatn': ['vazirmatn-rd', 'ui-sans-serif', 'system-ui']
+    },
     extend: {},
   },
   plugins: [],
