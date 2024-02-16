d77629fe59c880a1514fdef095d96da4f24df172
mostafa <nematpor46@gmail.com>
2024-02-16 18:20:31 +0330

    fix: change router history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import BlogsView from '../views/BlogsView.vue'
 import BlogView from '../views/BlogView.vue'
 import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
