d3f02381d0ec10832f0ce82e1668ce0f51701c93
mostafa <nematpor46@gmail.com>
2024-02-24 22:59:48 +0330

    Revert "test deploy action"
    This reverts commit 1d0bbfdac9aa4796e24170e1fbeae8a1d7dd0457.

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -9,7 +9,6 @@ const blogs = computed(() => {
 </script>
 
 <template>
-    <div>test deploy action</div>
     <div dir="rtl" class="container max-w-[580px] w-full mx-auto">
         <template v-for="blogItem in blogs" :key="blogItem.id">
             <BlogItem :blog="blogItem" />
