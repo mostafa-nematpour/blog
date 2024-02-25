6b9962197e43b669ebd9abc8f34eb0666bf41cd3
mostafa <nematpor46@gmail.com>
2024-02-25 16:04:27 +0330

    remove docs folder

--- docs/_nuxt/index.BtvWOZV0.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./index.vue.B5N4XKCC.js";import{N as t,t as e}from"./entry.BBACrwX3.js";import"./blog.Rwvay3Ok.js";const n={__name:"index",setup(o){return(_,a)=>(e(),t(r))}};export{n as default};
--- docs/_nuxt/index.DWreF4OF.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.vue.B5N4XKCC.js";import"./entry.BBACrwX3.js";import"./blog.Rwvay3Ok.js";export{o as default};
