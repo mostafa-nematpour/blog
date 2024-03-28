828c25b57cf4c05e9ae043206bddbd105bf1975d
Mostafa <nematpor46@gmail.com>
2024-03-29 01:22:56 +0330

    feat(cookie): add siteSettings

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+const settings = useCookie('siteSettings', {
+  default: () => ({
+    theme: 'dark',
+    lang: 'fa',
+    dir: 'rtl'
+  }),
+  maxAge: 100_000_000,
+  sameSite: 'lax'
+})
+
+const classObject = computed(() => {
+  const theme = settings.value.theme
+  return [theme, settings.value.dir == 'rtl' ? 'rtl' : 'ltr']
+})
 </script>
 
 <template>
-  <NuxtPage />
+  <NuxtPage :class="classObject" />
 </template>
-
