8e6d4ca5d40b92278e0cb4c0fbf0ff2e0da2b14b
Mostafa <nematpor46@gmail.com>
2024-04-22 13:52:18 +0330

    fix: favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,19 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+        { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#1f2667' },
+        { rel: 'shortcut icon', href: '/favicon/favicon.ico' }
+      ],
+      meta: [
+        { name: 'msapplication-TileColor', content: '#1f2667' },
+        { name: 'msapplication-config', content: '/favicon/browserconfig.xml' },
+        { name: 'theme-color', content: '#1f2667' }
+      ],
       script: [
         {
           hid: 'check-dark-mode',
