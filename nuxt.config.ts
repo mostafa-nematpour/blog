4fa7bfd235e0f4a19f25f9e54f5f80a28f5e5543
Mostafa <nematpor46@gmail.com>
2024-05-08 15:25:43 +0330

    feat: add vueuse/nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/turnstile'],
+  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/turnstile', '@vueuse/nuxt'],
   pinia: {
     storesDirs: ['./stores/**', './custom-folder/stores/**']
   },
