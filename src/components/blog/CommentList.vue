4f2e6ba4286470a3f89885ee30dfbe708028a49a
Mostafa <nematpor46@gmail.com>
2024-05-09 15:45:25 +0330

    fix: add empty List widget for comment list

--- src/components/blog/CommentList.vue
+++ src/components/blog/CommentList.vue
@@ -2,6 +2,7 @@
 import { useFetch } from '@vueuse/core'
 import CommentListItem from '~/components/blog/CommentListItem.vue'
 import CommentListLoading from './CommentListLoading.vue';
+import CommentListEmpty from './CommentListEmpty.vue';
 
 const props = defineProps(
   {
@@ -34,13 +35,17 @@ defineExpose({ refreshCommentList })
       <CommentListLoading />
     </div>
     <template v-else-if="data">
-      <div>لیست نظرات</div>
-      <br>
-      <ul>
-        <li v-for="(item, index) in data" :key="item.id">
-          <CommentListItem :item="item"></CommentListItem>
-        </li>
-      </ul>
+      <template v-if="data.length > 0">
+
+        <div>لیست نظرات</div>
+        <br>
+        <ul>
+          <li v-for="(item, index) in data" :key="item.id">
+            <CommentListItem :item="item"></CommentListItem>
+          </li>
+        </ul>
+      </template>
+      <CommentListEmpty v-else></CommentListEmpty>
     </template>
     <div v-else class="flex items-center flex-col">
       <span class="mb-2 text-2xl font-black text-c-text">
--- src/components/blog/CommentListEmpty.vue
+++ src/components/blog/CommentListEmpty.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-
+    <div class="flex items-center flex-col">
+        <span class="mb-2 text-2xl font-black text-c-text">
+            🗒️ برای این پست نظری ثبت نشده
+        </span>
+        <p class="text-xs">شما می‌توانید اولین نفر باشین</p>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
